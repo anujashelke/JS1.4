291836725f32ad0258113cdd41a12fc22779f136
anujashelke <anuja_shelke@outlook.com>
2017-05-30 15:41:36 +0530

    submitted

---
+++ app.js
@@ -0,0 +1,29 @@
+var Employee = [
+ "Anuja",
+ 25,
+ 25000,
+{
+city : "Amravati",
+state : "Maharashrta",
+pincode : 444604
+
+}
+
+]
+
+document.write("Name is :" + Employee[0] + "<br />");
+
+
+document.write("age is :" + Employee[1] + "<br />");
+
+
+document.write("salary is :" + Employee[2] + "<br />");
+
+
+document.write("City is :" + Employee[3].city + "<br />");
+
+
+document.write("State is :" + Employee[3].state + "<br />");
+
+
+document.write("Pincode is :" + Employee[3].pincode + "<br />");
